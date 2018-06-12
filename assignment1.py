3768901db790f766f1a88aa2886f02050729ee3b
surya-232 <saxenasurya001@gmail.com>
2018-06-12 15:53:23 +0530

    assignment 1

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+print("question1")
+print("surya saxena")
+print("\n")
+
+print("question2")
+x="Acad"
+y="View"
+print(x+y)
+print("\n")
+
+print("question3")
+print("enter the values")
+x=input("enter value of x:")
+y=input("enter value of y:")
+z=input("enter value of z:")
+
+print("the values of x,y,z is"+ ' ' +x + ' ' +y + ' ' +z + ' ' )
+print("\n")
+
+print("question4")
+print("let's get started")
+print("\n")
+
+print("question5")
+s="AcadView"
+Course="Python"
+fees=5000
+print("%s %s %d"%(s,Course,fees))
+print("\n")
+
+print("question6")
+name="TONY STARK"
+salary="100000"
+print("%s %d"%(name,salary))
