287ed98f26a8242d5101eb97b2c10452b86e5748
surya-232 <saxenasurya001@gmail.com>
2018-07-05 23:23:55 +0530

    file 19 added

---
+++ demo.py
@@ -0,0 +1,20 @@
+import numpy as np
+import sys
+import time
+
+#1
+a=np.array([1,2,3,4,5])
+print(a)
+print(type(a))
+
+print(a.nbytes)
+print(a.dtype)
+print(a.shape)
+
+print(a.item)
+print(a.size)
+print(a.ndim)
+
+#2
+
+#3
