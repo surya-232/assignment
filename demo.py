17c054379ffc6e24f33ba1444fdf85598e3ddb76
surya-232 <saxenasurya001@gmail.com>
2018-07-06 14:59:50 +0530

    file 20 added

--- demo.py
+++ demo.py
@@ -1,20 +1,21 @@
+import pandas as p
 import numpy as np
-import sys
-import time
 
-#1
-a=np.array([1,2,3,4,5])
-print(a)
-print(type(a))
+d = {
+    'name' : ['surya','ashwani','charu','shivam','karna'],
+    'age' : [19,20,20,21,21],
+    'mail_id' : ['saxenasurya001@gmail.com','ashwani6969@gmail.com','charuvashist@gmail.com','sainikalaunda@gmail.com','abikarna@gmail.com'],
+    'ph_number' : [9053408984,9053408998,9053408984,9053408984,9999999999]
+}
+df=p.DataFrame(d)
+print(df)
 
-print(a.nbytes)
-print(a.dtype)
-print(a.shape)
 
-print(a.item)
-print(a.size)
-print(a.ndim)
-
-#2
-
-#3
+df=p.read_csv('s.csv')
+print(df)
+print(df.head(5))
+print(df[0:5])
+print(df.head(10))
+print(df.describe())
+print(df.tail(5))
+print(df['Location'].describe())
